201839620fd4eb3c5ec15ffbbaf22e2bb5d9affb
1mshy <imshi1@protonmail.com>
2023-05-16 09:51:29 -0400

    Added some stuff there

---
+++ src/main/java/com/imshy/demo/FXMLExampleController.java
@@ -0,0 +1,22 @@
+package com.imshy.demo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class FXMLExampleController {
+    @FXML
+    public Text actiontarget;
+    public PasswordField passwordField;
+    public TextField userTextField;
+
+
+    @FXML
+    protected void handleSubmitButtonAction() {
+        System.out.println(userTextField.getText());
+        System.out.println(passwordField.getText());
+        actiontarget.setText("Sign in button pressed");
+    }
+}
