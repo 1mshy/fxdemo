fec1cfd8f39261b712c42095b96794c7a349703a
1mshy <imshi1@protonmail.com>
2023-05-16 10:18:42 -0400

    I love java fx this is actually SICK x2

--- src/main/java/com/imshy/demo/HelloApplication.java
+++ src/main/java/com/imshy/demo/HelloApplication.java
@@ -11,6 +11,7 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     private static TabPane tabPane = new TabPane();
@@ -27,7 +28,7 @@ public class HelloApplication extends Application {
 
         Tab tab1 = new Tab();
         tab1.setText("Welcome Screen");
-        tab1.setContent(FXMLLoader.load(this.getClass().getResource("fxml_example.fxml")));
+        tab1.setContent(load("fxml_example.fxml"));
         tabPane.getTabs().add(tab1);
         VBox vBox = new VBox(tabPane);
         scene = new Scene(vBox, 960, 600);
@@ -35,6 +36,7 @@ public class HelloApplication extends Application {
         stage.setTitle("JavaFX App");
         stage.show();
     }
+
     public static void accessAllowed() {
         tabPane.getTabs().remove(0);
         Tab tab2 = new Tab("Hello tab");
@@ -47,9 +49,10 @@ public class HelloApplication extends Application {
     public static void launch(String... args) {
         Application.launch(args);
     }
+
     public static Node load(String fxmlFile) {
         try {
-        return (Node) FXMLLoader.load(HelloApplication.class.getResource(fxmlFile));
+            return (Node) FXMLLoader.load(Objects.requireNonNull(HelloApplication.class.getResource(fxmlFile)));
         } catch (IOException e) {
             System.out.println("Error loading " + fxmlFile);
             throw new RuntimeException("Cannot load file " + fxmlFile);
